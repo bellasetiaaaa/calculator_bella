ff7aaf0d8753b41dd84af6c3f3a5dddf7039d774
bellasetiaaaa <manggalaredi61@gmail.com>
2023-11-23 07:52:34 +0700

    kalkulator

---
+++ main.js
@@ -0,0 +1,89 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+let input = "";
+
+for (let key of keys) {
+    const value = key.dataset.key;
+
+    key.addEventListener('click', () => {
+        if(value == "clear") {
+            input = "";
+            display_input.innerHTML = "";
+            display_output.innerHTML = "";
+        }else if (value == "backspace") {
+            input = input.slice(0, -1);
+            display_input.innerHTML = CleanInput(input);
+        }else if (value == "=") {
+            let result = eval(input);
+
+            display_output.innerHTML = CleanOutput(result);
+        }else if (value == "brackets") {
+            if (
+                input.indexOf("(") == -1 ||
+                input.indexOf("(") != -1 && 
+                input.indexOf(")") != -1 && 
+                input.lastIndexOf("(") < input.lastIndexOf(")")
+        ){
+                input += "(";
+        }else if (
+            input.indexOf("(") != -1 && 
+            input.indexOf(")") == -1 ||
+            input.indexOf("(") != -1 &&
+            input.indexOf(")") != -1 &&
+            input.lastIndexOf("(") >input.lastIndexOf(")")
+        ) {
+            input += ")";
+        }
+        display_input.innerHTML = CleanInput(input);
+        }else {
+            input += value;
+        display_input.innerHTML = CleanInput(input);
+        }
+    })
+}
+
+function CleanInput(input) {
+    let input_array = input.split("");
+    let input_array_length = input_array_length;
+
+    for (let i = 0; i < input_array_length; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = '<span class="operator">x</span>';
+        }else if (input_array[i] == "/") {
+            input_array[i] = '<span class="operator">:</span>'; 
+        }else if (input_array[i] == "+") {
+            input_array[i] = '<span class="operator">+</span>'; 
+        }else if (input_array[i] == "-") {
+            input_array[i] = '<span class="operator">-</span>'; 
+        }else if (input_array[i] == "(") {
+            input_array[i] = '<span class="brackets">(</span>'; 
+        }else if (input_array[i] == ")") {
+            input_array[i] = '<span class="brackets">)</span>'; 
+        }else if (input_array[i] == "%") {
+            input_array[i] = '<span class="precent">%</span>';
+        }
+    }
+    
+    return input_array.join("");
+}
+
+function CleanInput (output) {
+    let output_string = output.toString();
+    let decinal = output_string.split(".")[1];
+    output_string = output_string.split(".")[0];
+
+    let output_array = output_string.split("");
+
+    if (output_array.length > 3) {
+        for (let i = output_array.length - 3; i > 0; i -= 3) { 
+            output_array.splice(i, 0, ","); 
+        }
+    }
+    if (decinal) {
+        output_array.push(".");
+        output_array.push(decinal);
+    }
+    return output_array.join("");
+}
